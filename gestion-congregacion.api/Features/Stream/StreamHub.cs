4a0b346e3b7ec4b1b80b8912e665bc3b5b68397a
Gabriel Macus <gabrielmacus@gmail.com>
2024-07-06 20:43:36 -0300

    Agregar archivos de proyecto.

---
+++ gestion-congregacion.api/Features/Stream/StreamHub.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using StackExchange.Redis;
+
+namespace gestion_congregacion.api.Features.Stream
+{
+    public class StreamHub : Hub
+    {
+        const string VIEWERS_HASH_KEY = "viewers";
+
+        private readonly IConnectionMultiplexer _redis;
+        public StreamHub(IConnectionMultiplexer redis)
+        {
+            _redis = redis;
+        }
+
+        public async Task SendViewers()
+        {
+            var viewers = await _redis.GetDatabase().HashGetAllAsync(VIEWERS_HASH_KEY);
+            var viewersList = viewers.Select(x =>
+            {
+                var split = x.Value.ToString().Split(":");
+                return new Dictionary<string, dynamic>() {
+                    ["name"] = split[0],
+                    ["participants"] = int.Parse(split[1])
+                };
+            });
+            await Clients.All.SendAsync("Viewers", viewersList);
+
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var name = Context?.GetHttpContext()?.GetRouteValue("Name")?.ToString();
+            var participants = Context?.GetHttpContext()?.GetRouteValue("Participants")?.ToString();
+
+            await _redis.GetDatabase().HashSetAsync(VIEWERS_HASH_KEY, Context?.ConnectionId, $"{name}:{participants}");
+            await SendViewers();
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await _redis.GetDatabase().HashDeleteAsync(VIEWERS_HASH_KEY, Context?.ConnectionId);
+            await SendViewers();
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
+
---
+++ gestion-congregacion.api/Program.cs
@@ -0,0 +1,57 @@
+using gestion_congregacion.api.Features.Stream;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy => {
+        policy.WithOrigins(builder.Configuration.GetValue<string>("AllowedOrigins"));
+        policy.AllowAnyMethod();
+        policy.AllowAnyHeader();
+        policy.AllowCredentials();
+    });
+    /*
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+    policy =>
+    {
+        policy.WithOrigins("http://example.com",
+                            "http://www.contoso.com");
+    });*/
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+
+var multiplexer = ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis"));
+builder.Services.AddSingleton<IConnectionMultiplexer>(multiplexer);
+multiplexer.GetServer(multiplexer.GetEndPoints().First()).FlushDatabase();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<StreamHub>("/hubs/stream/{Name}/{Participants:int}");
+
+app.UseCors();
+
+app.Run();
